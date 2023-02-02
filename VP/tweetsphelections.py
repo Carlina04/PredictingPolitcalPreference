5885b3fce660ebc4c94c34f5d530f9b387806c11
samkayes <samanthasubaan@gmail.com>
2023-02-03 00:04:15 +0800

    preprocessing test
    using the phelections hashtag

--- VP/tweetsphelections.py
+++ VP/tweetsphelections.py
@@ -1,5 +1,6 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import nltk
 
 query = "#PHElections2022 until:2022-05-07 since:2022-02-08"
 
@@ -17,4 +18,30 @@ for tweet in sntwitter.TwitterSearchScraper(query).get_items():
 df = pd.DataFrame(tweets, columns=['Date', 'User','Tweet'])
 
 print(df)
-df.to_csv('phelec.csv')
+df.to_csv('VP/phelec.csv')
+
+final=pd.read_csv('VP/phelec.csv')
+X=final['Tweet']
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+import string
+from nltk.stem import PorterStemmer
+
+stop_words=stopwords.words('english')
+punct=string.punctuation
+stemmer=PorterStemmer()
+
+import re
+cleaned_data=[]
+for i in range(len(X)):
+    tw=re.sub('[^a-zA-Z]',' ',X.iloc[i])
+    tw=tw.lower().split()
+    tw=[stemmer.stem(word) for word in tw if (word not in stop_words) and (word not in punct)]
+    tw=' '.join(tw)
+    cleaned_data.append(tw)
+
+df2 = pd.DataFrame(cleaned_data)
+
+print(df2)
+df2.to_csv('VP/test.csv')
